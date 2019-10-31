77e5807b2cc48fb41dc5a2ca60f36e1081a7c17d
Isaac Dias <isaacardias@gmail.com>
2019-10-30 21:10:10 -0300

    criando classe e objeto

---
+++ src/objetoseclassees/Pessoa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetoseclassees;
+
+/**
+ *
+ * @author isaac
+ */
+public class Pessoa {
+    
+    int codigo;
+    String nome;
+    
+    public Pessoa(int codigo, String nome){
+        this.codigo=codigo;
+        this.nome=nome;
+    }
+    
+}
---
+++ src/objetoseclassees/PessoaApp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetoseclassees;
+
+/**
+ *
+ * @author isaac
+ */
+public class PessoaApp {
+    public static void main(String[] args){
+        
+        Pessoa pessoa1 = new Pessoa (1, "isaac");
+        Pessoa pessoa2 = new Pessoa (2, "rafael");
+        
+    }
+    
+}
